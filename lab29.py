890eac81e0ec411bb60fedb73bdc64cd4e9b7456
Sangamesh <94958665+sangamesh-10@users.noreply.github.com>
2022-09-07 13:27:44 +0530

    Update lab29.py

--- lab29.py
+++ lab29.py
@@ -1,12 +1,7 @@
 import sys
-import os
-# program should be executed in command prompt or terminal, to give filename as an command line argument..
 try:
-    with open(sys.argv[1],'r') as f:
-        cwd = os.getcwd()
-        print("file path: %s" % cwd+"\%s"%sys.argv[1])
-        print("file opened..")
-        print("file content---->")
-        print(f.read())
-except:
-    print("some error occured..")
+	fp = open(sys.argv[1],'r')
+	fp.seek(int(sys.argv[2]))
+	print(fp.read())
+except FileNotFoundError :
+	print("there no file with such name..")
